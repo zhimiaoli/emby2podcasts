f35a0bc5d8716221bfc8796037b00c0b581fb49d
lizhimiao <untildied@gmail.com>
2023-02-07 19:20:58 +0800

    first working version

--- app.py
+++ app.py
@@ -1,6 +1,78 @@
-from flask import Flask
+from flask import Flask,abort,redirect,Response
+from jinja2 import Template
+import requests
+import os
+import json
+import sys
 app = Flask(__name__)
+
+podcastTpl = """<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+<channel>
+<title>{{basicInfoData['Name']}}</title>
+<link>https://github.com/zhimiaoli/emby2podcasts</link>
+<language>zh-cn</language>
+<itunes:subtitle>{{basicInfoData['Name']}}</itunes:subtitle>
+<itunes:author>{{basicInfoData.get("AlbumArtist","")}}</itunes:author>
+<itunes:image href="{{emby_file_server}}/emby/Items/{{basicInfoData["Id"]}}/Images/Primary?width=500&height=500"/>
+<itunes:summary><![CDATA[ {{basicInfoData.get("Overview","")}}]]></itunes:summary>
+<description><![CDATA[{{basicInfoData.get("Overview","")}}]]></description>
+<itunes:owner>
+    <itunes:name>Private Use</itunes:name>
+    <itunes:email>me@example.com</itunes:email>
+</itunes:owner>
+<itunes:explicit>no</itunes:explicit>
+
+{% for ep in eps %}
+<item>
+    <title>{{ep["IndexNumber"]}}{{"."+ep["Name"]}}</title>
+    <itunes:summary><![CDATA[ {{ep.get("Overview","")}}]]></itunes:summary>
+    <description><![CDATA[ {{ep.get("Overview","")}}]]></description>
+    <enclosure url="{{baseURL}}{{'/audio/'+ep['Id']}}" type="audio/mpeg"></enclosure>
+    <pubDate>Tue, 07 Feb 2023 16:01:07 +0000</pubDate>
+    <itunes:author>{{basicInfoData.get("AlbumArtist","Unknow")}}</itunes:author>
+    <itunes:duration>{{ep["RunTimeTicks"]/10000000 | int}}</itunes:duration>
+    <itunes:explicit>no</itunes:explicit>
+    <guid>{{ep["ServerId"]}}-{{ep["Id"]}}</guid>
+</item> 
+{% endfor %}
+</channel>
+</rss>
+"""
+
+def loadConfig():
+    if os.path.exists("config.json"): 
+        configfilename = "config.json"
+        configData = open(configfilename,"r",encoding="utf-8").read()
+        return json.loads(configData)
+    elif os.path.exists("/config/config.json"):
+        configfilename = "/config/config.json"
+        configData = open(configfilename,"r",encoding="utf-8").read()
+        return json.loads(configData)
+    else:
+        sys.exit(0)
+
+config = loadConfig()
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello, It works!'
+
+
+@app.route("/podcast/<id>")
+def podcast(id):
+    # 生成podcast的播客
+    r = requests.session()
+    basicInfo = r.get("{emby_api_server}/emby/Users/{user_id}/Items/{id}?api_key={api_key}".format(emby_api_server=config["emby_api_server"],user_id=config["user_id"],id=id,api_key=config["api_key"]))
+    eposides = r.get("{emby_api_server}/emby/Items?ParentId={id}&api_key={api_key}".format(emby_api_server=config["emby_api_server"],api_key=config["api_key"],id=id))
+    if basicInfo.status_code == 200 and eposides.status_code == 200:
+        basicInfoData = basicInfo.json()
+        eposidesList = eposides.json()
+        xmlStr = Template(podcastTpl).render(basicInfoData=basicInfoData,eps=eposidesList["Items"],baseURL = config["baseURL"],emby_file_server=config["emby_file_server"])
+    else:
+        return abort(basicInfo.status_code)
+    return Response(xmlStr, mimetype='application/xml')
+
+@app.route("/audio/<audioid>")
+def serverAudio(audioid):
+    return redirect("{emby_file_server}/emby/Audio/{audioid}/stream?api_key={api_key}&static=true".format(api_key=config["api_key"],emby_file_server=config["emby_file_server"],audioid=audioid),302)
