c93e0bc551412e8beaf928c19bb95eb19b302e55
lizhimiao <untildied@gmail.com>
2023-11-10 20:00:42 +0800

    added video support

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ podcastTpl = """<?xml version="1.0" encoding="UTF-8"?>
 <link>https://github.com/zhimiaoli/emby2podcasts</link>
 <language>zh-cn</language>
 <itunes:subtitle>{{basicInfoData['Name']|e}}</itunes:subtitle>
-<itunes:author>{{basicInfoData.get("AlbumArtist","")}}</itunes:author>
+<itunes:author>{{basicInfoData.get("AlbumArtist","John Doe")}}</itunes:author>
 <itunes:image href="{{emby_image_server}}/emby/Items/{{basicInfoData["Id"]}}/Images/Primary"/>
 <itunes:summary><![CDATA[ {{basicInfoData.get("Overview","")}}]]></itunes:summary>
 <description><![CDATA[{{basicInfoData.get("Overview","")}}]]></description>
@@ -26,10 +26,10 @@ podcastTpl = """<?xml version="1.0" encoding="UTF-8"?>
 {% set count = namespace(value=1) %}
 {% for ep in eps %}
 <item>
-    <title>{{ep.get("IndexNumber",count.value)}}{{"."+ep["Name"]|e}}</title>
+    <title>{% if ep.get("ParentIndexNumber","")%}{{ep.get("ParentIndexNumber","")}}-{% endif %}{{ep.get("IndexNumber",count.value)}}{{"."+ep["Name"]|e}}</title>
     <itunes:summary><![CDATA[ {{ep.get("Overview","")}}]]></itunes:summary>
     <description><![CDATA[ {{ep.get("Overview","")}}]]></description>
-    <enclosure url="{{baseURL}}{{'/audio/'+ep['Id']}}.{{ep['Container']}}" type="audio/{% if ep["Container"] == "m4a" %}mp4{% else %}mpeg{% endif %}"></enclosure>
+    <enclosure url="{{baseURL}}{{'/audio/'+ep['Id']}}.{{ep['Container']}}" type="{% if ep["Container"] == "m4a" %}audio/mp4{% else %}video/mpeg{% endif %}"></enclosure>
     <pubDate>{{ep["pubDate"]}} GMT</pubDate>
     <itunes:author>{{basicInfoData.get("AlbumArtist","Unknow")}}</itunes:author>
     <itunes:duration>{{(ep["RunTimeTicks"]/10000000)|int}}</itunes:duration>
@@ -93,6 +93,35 @@ def serverAudio(audiofile):
     if container == "mp3" or container == "m4a":
         print("mp3 container, direct stream.")
         return redirect("{emby_file_server}/emby/Audio/{audioid}/stream?api_key={api_key}&static=true".format(api_key=config["api_key"],emby_file_server=config["emby_file_server"],audioid=audioid),302)
+    if container == "mp4":
+        print("mp3 container, direct stream.")
+        return redirect("{emby_file_server}/emby/videos/{audioid}/stream.mp4?api_key={api_key}&static=true".format(api_key=config["api_key"],emby_file_server=config["emby_file_server"],audioid=audioid),302)
     else:
         print("other container, transcoding.")
         return redirect("{emby_file_server}/emby/Audio/{audioid}/stream.mp3?api_key={api_key}".format(api_key=config["api_key"],emby_file_server=config["emby_file_server"],audioid=audioid),302)
+    
+@app.route("/tv/<id>")
+def tv(id):
+    # 生成podcast的播客
+    r = requests.session()
+    basicInfo = r.get("{emby_api_server}/emby/Users/{user_id}/Items/{id}?api_key={api_key}".format(emby_api_server=config["emby_api_server"],user_id=config["user_id"],id=id,api_key=config["api_key"]))
+
+    eposides = r.get("{emby_api_server}/emby/Items?ParentId={id}&api_key={api_key}&Recursive=true&IncludeItemTypes=Episode&Fields=BasicSyncInfo%2CContainer".format(emby_api_server=config["emby_api_server"],api_key=config["api_key"],id=id))
+
+    if basicInfo.status_code == 200 and eposides.status_code == 200:
+        basicInfoData = basicInfo.json()
+        eposidesList = eposides.json()
+        epindex = 1
+        for ep in eposidesList["Items"]:
+            pubDate = datetime(2023,2,8,2,23,3) - timedelta(days=epindex)
+            #using a fixed date,so no need to refresh the rss file,this is the date I finished a first working version
+            #of this simple program. no timezone info was provided.
+            ep["pubDate"] = pubDate.strftime("%a, %d %b %Y %H:%M:%S")
+            epindex += 1
+        xmlStr = Template(podcastTpl).render(basicInfoData=basicInfoData,
+                                             eps=eposidesList["Items"],
+                                             baseURL = config["baseURL"],
+                                             emby_file_server=config["emby_file_server"],emby_image_server=config["emby_image_server"])
+    else:
+        return abort(basicInfo.status_code)
+    return Response(xmlStr, mimetype='application/xml')
